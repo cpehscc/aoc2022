12a15c549528e878ac50a0fb89d2f189389a651f
Evan  Tao <52547607+cpehscc@users.noreply.github.com>
2022-12-04 22:22:09 +0000

    day 5

---
+++ src/Day5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Day5 {
+  
+  public static void part1() throws FileNotFoundException {
+    Scanner sc = new Scanner(new File("./inputs/input5.txt"));
+    while (sc.hasNextLine()) {
+      String next = sc.nextLine();
+    }
+    sc.close();
+  }
+
+  public static void part2() throws FileNotFoundException {
+    Scanner sc = new Scanner(new File("./inputs/input5.txt"));
+    while (sc.hasNextLine()) {
+      String next = sc.nextLine();
+    }
+    sc.close();
+  }
+  
+}
