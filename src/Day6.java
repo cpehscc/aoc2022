30770bccc59381570631627ccb8fa33c6861e216
Evan  Tao <52547607+cpehscc@users.noreply.github.com>
2022-12-06 13:30:09 +0000

    day 6

--- src/Day6.java
+++ src/Day6.java
@@ -9,16 +9,52 @@ class Day6 {
   
   public void part1() throws FileNotFoundException {
     Scanner sc = new Scanner(new File("./inputs/input6.txt"));
-    while (sc.hasNextLine()) {
-      String next = sc.nextLine();
+    char[] next = sc.nextLine().toCharArray();
+    Set<Character> chars;
+    boolean distinct = false;
+    int i = 4;
+    while (!distinct) {
+      chars = new HashSet<Character>();
+      chars.add(next[i-4]);
+      chars.add(next[i-3]);
+      chars.add(next[i-2]);
+      chars.add(next[i-1]);
+      if (chars.size() == 4) {
+        System.out.println(i);
+        distinct = true;
+      }
+      i++;
     }
     sc.close();
   }
 
   public void part2() throws FileNotFoundException {
     Scanner sc = new Scanner(new File("./inputs/input6.txt"));
-    while (sc.hasNextLine()) {
-      String next = sc.nextLine();
+    char[] next = sc.nextLine().toCharArray();
+    Set<Character> chars;
+    boolean distinct = false;
+    int i = 14;
+    while (!distinct) {
+      chars = new HashSet<Character>();
+      chars.add(next[i-14]);
+      chars.add(next[i-13]);
+      chars.add(next[i-12]);
+      chars.add(next[i-11]);
+      chars.add(next[i-10]);
+      chars.add(next[i-9]);
+      chars.add(next[i-8]);
+      chars.add(next[i-7]);
+      chars.add(next[i-6]);
+      chars.add(next[i-5]);
+      chars.add(next[i-4]);
+      chars.add(next[i-3]);
+      chars.add(next[i-2]);
+      chars.add(next[i-1]);
+      if (chars.size() == 14) {
+        System.out.println(i);
+        distinct = true;
+      }
+      i++;
     }
     sc.close();
   }
